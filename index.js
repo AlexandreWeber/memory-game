944a306fc5e7613dd01e7f142a9750db050221de
Your Name <alexandre.dalabona@gmail.com>
2020-03-26 00:13:06 -0300

    Novos hérois

--- index.js
+++ index.js
@@ -1,179 +1,116 @@
-const heroes = [{
-    id: 1,
-    img: './assets/batman.png',
-    name: 'Batman'
-},
-{
-    id: 2,
-    img: './assets/daredevil.png',
-    name: 'Daredevil'
-},
-{
-    id: 3,
-    img: './assets/flash.png',
-    name: 'Flash'
-},
-{
-    id: 4,
+const heroes = Game.getHeroes();
 
-    img: './assets/woman.png',
-    name: 'Woman'
-}, {
-    id: 5,
-    img: './assets/hellboy.png',
-    name: 'Hellboy'
-}, {
-    id: 6,
-    img: './assets/wonder.png',
-    name: 'Wonder Woman'
-}, {
-    id: 7,
-    img: './assets/batman.png',
-    name: 'Batman'
-},
-{
-    id: 8,
-    img: './assets/daredevil.png',
-    name: 'Daredevil'
-},
-{
-    id: 9,
-    img: './assets/flash.png',
-    name: 'Flash'
-},
-{
-    id: 10,
-    img: './assets/woman.png',
-    name: 'Woman'
-}, {
-    id: 11,
-    img: './assets/hellboy.png',
-    name: 'Hellboy'
-}, {
-    id: 12,
-    img: './assets/wonder.png',
-    name: 'Wonder Woman'
+let gameState = {
+	card1: {},
+	card2: {},
+	counter: 0,
+	movements: 0,
+	interval: undefined,
+	okCounter: 0
 }
-];
-
-let card1 = {};
-let card2 = {};
-let counter = 0;
-let movements = 0;
-let interval = undefined;
-let okCounter = 0;
 
 function onLoad() {
-    this.generateGame(heroes);
+	this.generateGame(heroes);
 
-    document.getElementById("new-game").addEventListener("click", () => this.generateGame(heroes));
+	document.getElementById("new-game").addEventListener("click", () => this.generateGame(heroes));
 }
 
 function generateGame(heroes) {
-    let htmlContent = '';
-    counter = 0;
-    movements = 0;
-    okCounter = 0;
-    clearInterval(interval);
-    clearMovements();
-    
-    heroes = Game.shuffle(heroes);
+	let htmlContent = '';
 
-    heroes.map((heroe) => {
-        htmlContent += Game.getHtmlContent(heroe);
-    });
+	clearGameState();
 
-    Game.updateContent(htmlContent);
+	heroes = Game.shuffle(heroes);
 
-    this.hideHeroes();
+	heroes.map((heroe) => {
+		htmlContent += Game.getHtmlContent(heroe);
+	});
 
-    this.handleClick();
-
-    this.startTimer();
+	Game.updateContent(htmlContent);
+	Game.hideHeroes();
+	this.handleClick();
+	this.startTimer();
 }
 
-function hideHeroes() {
-    document.querySelectorAll('.card-img-top')
-        .forEach((heroe) => {
-            heroe.src = "./assets/default.png"
-        });
-}
-
-function hideHeroe(id) {
-    document.getElementById(`${id}`).src = "./assets/default.png";
+function clearGameState() {
+	gameState.counter = 0;
+	gameState.movements = 0;
+	gameState.okCounter = 0;
+	clearInterval(gameState.interval);
+	clearMovements();
 }
 
 function handleClick() {
-    document.querySelectorAll('.card-img-top')
-        .forEach((heroe) => {
-            heroe.addEventListener('click', (event) => {
-                if (card1.id && card2.id || (card1.id === +event.target.id || card2.id === +event.target.id)
-                    || event.target.src.indexOf("default") === -1) {
-                    return;
-                }
-            
-                this.incrmentMovements();
+	document.querySelectorAll('.memory-card')
+		.forEach((heroe) => {
+			heroe.addEventListener('click', (event) => {
+				if (gameState.card1.id && gameState.card2.id || (gameState.card1.id === +event.target.id || gameState.card2.id === +event.target.id) ||
+					event.target.src.indexOf("default") === -1) {
+					return;
+				}
 
-                const h = heroes.find(heroe => heroe.id === +event.target.id)
-                event.target.src = h.img;
+				this.incrementMovements();
 
-                if (!card1.id) {
-                    card1 = {
-                        id: h.id,
-                        name: h.name
-                    };
-                } else {
-                    card2 = {
-                        id: h.id,
-                        name: h.name
-                    };
-                }
+				const h = heroes.find(heroe => heroe.id === +event.target.id)
+				event.target.src = h.img;
 
-                if (card1.id && card2.id) {
-                    if (card1.name === card2.name) {
-                        card1 = {};
-                        card2 = {};
-                        okCounter += 1;
+				if (!gameState.card1.id) {
+					gameState.card1 = {
+						id: h.id,
+						name: h.name
+					};
+				} else {
+					gameState.card2 = {
+						id: h.id,
+						name: h.name
+					};
+				}
 
-                        if (okCounter === 6) {
-                            this.stopGame();
-                        }
-                    } else if (card2.name) {
-                        setTimeout(() => {
-                            this.hideHeroe(card1.id);
-                            this.hideHeroe(card2.id);
-                            card1 = {};
-                            card2 = {};
-                        }, 500);
-                    }
-                }
-            })
-        });
+				if (gameState.card1.id && gameState.card2.id) {
+					if (gameState.card1.name === gameState.card2.name) {
+						gameState.card1 = {};
+						gameState.card2 = {};
+						gameState.okCounter += 1;
+
+						if (gameState.okCounter === 6) {
+							this.stopGame();
+						}
+					} else if (gameState.card2.name) {
+						setTimeout(() => {
+							Game.hideHeroe(gameState.card1.id);
+							Game.hideHeroe(gameState.card2.id);
+							gameState.card1 = {};
+							gameState.card2 = {};
+						}, 500);
+					}
+				}
+			})
+		});
 }
 
 function startTimer() {
-    interval = setInterval(() => {
-        document.getElementById('timer').innerHTML = counter;
+	gameState.interval = setInterval(() => {
+		document.getElementById('timer').innerHTML = gameState.counter;
 
-        counter++;
-    }, 1000);
+		gameState.counter++;
+	}, 1000);
 }
 
 function stopGame() {
-    counter = 0;
-    movements = 0;
-    okCounter = 0;
-    clearInterval(interval);
+	gameState.counter = 0;
+	gameState.movements = 0;
+	gameState.okCounter = 0;
+	clearInterval(gameState.interval);
 }
 
-function incrmentMovements() {
-    movements += 1;
+function incrementMovements() {
+	gameState.movements += 1;
 
-    document.getElementById('movements').innerHTML = movements;
+	document.getElementById('movements').innerHTML = gameState.movements;
 }
 
 function clearMovements() {
-    document.getElementById('movements').innerHTML = 0;
+	document.getElementById('movements').innerHTML = 0;
 }
 
 window.onload = onLoad;
--- src/game.js
+++ src/game.js
@@ -1,7 +1,7 @@
 class Game {
 	static getHtmlContent(heroe) {
 		return `
-			<div class="col-4 col-md-3">
+			<div class="col-2 col-md-2">
 				<div class="card memory-card">
 					<img id="${heroe.id}" src="${heroe.img}" class="card-img-top"/>
 				</div>
@@ -9,15 +9,126 @@ class Game {
 		`;
 	}
 
+	static getHeroes() {
+		return [{
+			id: 1,
+			img: './assets/batman.png',
+			name: 'Batman'
+		}, {
+			id: 2,
+			img: './assets/thor.png',
+			name: 'Thor'
+		}, {
+			id: 3,
+			img: './assets/flash.png',
+			name: 'Flash'
+		}, {
+			id: 4,
+			img: './assets/woman.png',
+			name: 'Woman'
+		}, {
+			id: 5,
+			img: './assets/hellboy.png',
+			name: 'Hellboy'
+		}, {
+			id: 6,
+			img: './assets/wonder.png',
+			name: 'Wonder Woman'
+		}, {
+			id: 7,
+			img: './assets/daredevil.png',
+			name: 'Daredevil'
+		}, {
+			id: 8,
+			img: './assets/groot.png',
+			name: 'Groot'
+		}, {
+			id: 9,
+			img: './assets/iron.png',
+			name: 'Iron'
+		}, {
+			id: 10,
+			img: './assets/spider.png',
+			name: 'Spider-Man'
+		}, {
+			id: 11,
+			img: './assets/captain.png',
+			name: 'Captain'
+		}, {
+			id: 12,
+			img: './assets/deadpool.png',
+			name: 'Deadpool'
+		}, {
+			id: 13,
+			img: './assets/batman.png',
+			name: 'Batman'
+		}, {
+			id: 14,
+			img: './assets/thor.png',
+			name: 'Thor'
+		}, {
+			id: 15,
+			img: './assets/flash.png',
+			name: 'Flash'
+		}, {
+			id: 16,
+			img: './assets/woman.png',
+			name: 'Woman'
+		}, {
+			id: 17,
+			img: './assets/hellboy.png',
+			name: 'Hellboy'
+		}, {
+			id: 18,
+			img: './assets/wonder.png',
+			name: 'Wonder Woman'
+		}, {
+			id: 19,
+			img: './assets/daredevil.png',
+			name: 'Daredevil'
+		}, {
+			id: 20,
+			img: './assets/groot.png',
+			name: 'Groot'
+		}, {
+			id: 21,
+			img: './assets/iron.png',
+			name: 'Iron'
+		}, {
+			id: 22,
+			img: './assets/spider.png',
+			name: 'Spider-Man'
+		}, {
+			id: 23,
+			img: './assets/captain.png',
+			name: 'Captain'
+		}, {
+			id: 24,
+			img: './assets/deadpool.png',
+			name: 'Deadpool'
+		}];
+	}
+
+	static hideHeroes() {
+		document.querySelectorAll('.card-img-top')
+			.forEach((heroe) => {
+				heroe.src = "./assets/default.png"
+			});
+	}
+
+	static hideHeroe(id) {
+		document.getElementById(`${id}`).src = "./assets/default.png";
+	}
+
 	static updateContent(htmlContent) {
 		const content = document.getElementById('content');
 
-		content.innerHTML = htmlContent	;
+		content.innerHTML = htmlContent;
 	}
 
 	static shuffle(heroes) {
 		const newHeroes = [];
-		const indexes = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+		const indexes = heroes.map((heroe, index) => index);
 		let total = 0;
 
 		while (total < heroes.length) {
